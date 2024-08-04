13600b442a937891b48fd282a08e6aab38d376c9
ShuntaToda <todashuntawork@gmail.com>
2024-08-04 12:44:38 +0900

    setting theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import "@fontsource/roboto/400.css";
+import { Header } from "./components/Header";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
-  return <></>;
+  const theme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#5bc7d2",
+      },
+      secondary: {
+        main: "#c5d4d8",
+      },
+      warning: {
+        main: "#f05435",
+      },
+    },
+  });
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
