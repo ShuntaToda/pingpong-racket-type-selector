6113b9cead4501d12c505391015d6d4a5a264462
ShuntaToda <todashuntawork@gmail.com>
2024-08-04 12:44:45 +0900

    create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            marginLeft={8}
+            paddingY={3}
+            color={"white"}
+            variant="h4"
+            fontWeight={"bold"}
+            component="h1"
+            sx={{ flexGrow: 1 }}
+          >
+            卓球ラケット選択 App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
